b13884597c5bb1702f633864bdaea08017ba40c7
Iryna Yehorova <yehorova.dev@gmail.com>
2024-10-07 23:12:37 +0300

    make build

---
+++ index.6eb1527d.js
@@ -0,0 +1,2 @@
+function e(){var e=window.innerWidth-document.documentElement.clientWidth,n=document.querySelector(".page__menu");n&&(n.style.width="calc(100vw - ".concat(e,"px)"))}window.addEventListener("load",e),window.addEventListener("resize",e);
+//# sourceMappingURL=index.6eb1527d.js.map
